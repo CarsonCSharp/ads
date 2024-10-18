ae1c54418103bb1a5033c5dbe5e71dbebd552dd3
TheThirdElement <rpnmndl0@gmail.com>
2024-10-18 13:10:23 +0530

    Create keep_alive.py
    Webserver file that enables users to keep the code running forever using uptimer websites.

---
+++ keep_alive.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def main():
+    # Display the message to indicate that the user can now set up UptimeRobot
+     return '''
+         <h1>Logged into account, use uptimer now!</h1>
+         <br>
+         <form action="https://www.youtube.com/@thethirdelement" method="get">
+             <button type="submit">The Third Element YouTube</button>
+         </form>
+         '''
+
+def run():
+     # Run the web server on the given host and port
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+     # Start the web server in a separate thread to keep the Replit project alive
+    server = Thread(target=run)
+    server.start()
